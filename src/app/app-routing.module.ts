3b907512739b28f100935c818661b9717e536aec
Vasko <vasko77@yahoo.gr>
2017-10-27 19:53:40 +0300

    1st commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VoucherListComponent } from './voucher-list/voucher-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: VoucherListComponent },
+  { path: '', redirectTo: 'list', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VoucherListComponent } from './voucher-list/voucher-list.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoucherListComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
